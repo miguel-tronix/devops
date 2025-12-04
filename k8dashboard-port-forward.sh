bfb4085a9d7dfe3109c1e2b52aac6acae279d428
Miguel de Sousa <miguels@quantanco.co>
2025-12-05 09:00:36 +1100

    fix:

--- k8dashboard-port-forward.sh
+++ k8dashboard-port-forward.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-microk8s.kubectl -n kubernetes-dashboard port-forward svc/kubernetes-dashboard-kong-proxy 8443:443 2> /dev/null
+if [ -z $1 ];then 
+	kubectl -n kubernetes-dashboard rollout restart deployment kubernetes-dashboard-kong
+	sleep 10
+fi
+kubectl -n kubernetes-dashboard port-forward svc/kubernetes-dashboard-kong-proxy 8443:443 2> /dev/null
+
