8d710a42c2dc7eb5019b21828060b1a56346cffd
miguel-tronix <miguel.tronix@gmail.com>
2026-04-08 06:41:20 +1000

    fastapi argo deployment

--- k8dashboard-port-forward.sh
+++ k8dashboard-port-forward.sh
@@ -2,7 +2,7 @@
 
 echo "Starting persistent port-forward for Kubernetes Dashboard on port 14333..."
 while true; do
-  kubectl -n kubernetes-dashboard port-forward svc/kubernetes-dashboard 14333:443
+  kubectl -n kubernetes-dashboard port-forward svc/kubernetes-dashboard-kong-proxy 14333:443
   echo "Port-forward exited with code $?. Restarting in 5 seconds..."
   sleep 5
 done
